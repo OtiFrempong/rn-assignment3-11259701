998ec605b717820f2cd8698cb3b3e65ab685377e
Serwaa19 <motifrempong@gmail.com>
2024-06-02 16:20:36 +0000

    Adding images to assets

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, ScrollView} from 'react-native';
+import myImage from './assets/Frame 1.png';
+import mySearch from './assets/Search.png';
+import OngoingTasks from './components/ongoingTasks';
+import Categories from './components/categories';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <Text>Hello Serwaa</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView  contentContainerStyle={styles.contentContainer} style={styles.container}>
+      <Image source={myImage} style={styles.headerTitle}/>
+      <Image source={mySearch} style={styles.searchInput}/>
+      <Text style={styles.header}>Categories</Text>
+      <Categories />
+      <OngoingTasks/>
+    
+    </ScrollView>
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8D1BA',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F7F0E8',
   },
+  contentContainer: {
+    alignItems: 'center',
+    paddingBottom: 20,
+    paddingTop: 50,
+  },
+  headerTitle: {
+    fontSize: 24,
+    marginTop:20,
+    padding:10,
+    fontWeight: 'bold',
+  },
+  header: {
+    fontSize: 24,
+    marginTop:20,
+    padding:10,
+    marginLeft: -250,
+    fontWeight: 'bold',
+  },
+  searchInput: {
+    marginTop: 40,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  exerciseStudy: {
+     marginTop: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius:5
+  }
 });
---
+++ components/ongoingTasks.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { StyleSheet, Text, View,FlatList} from 'react-native';
+
+
+function OngoingTasks() {
+    
+
+  return (
+    <View style={styles.taskContainerWrapper}>
+    <View style={styles.taskContainer}>
+      <Text style={styles.taskTitle}>Ongoing Tasks</Text>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}>Mobile App development</Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}>Web development</Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}>Push Ups</Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}></Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}></Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}></Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}></Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}></Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}></Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}></Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}></Text>
+      </View>
+      <View style={styles.taskpaper}>
+        <Text style={styles.taskName}></Text>
+      </View>
+    </View>
+  </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  todayTasks: {
+    width: 154,
+    fontSize: 15,
+  },
+  taskpaper: {
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 3,
+    marginVertical: 5,
+    width: '100%',
+    alignSelf: 'center',
+    height: 110,
+    justifyContent: 'center',
+    borderColor: '#E8D1BA',
+  },
+  taskName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingLeft: 10,
+  },
+  taskContainerWrapper: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  taskContainer: {
+    width: '90%',
+    backgroundColor: '#F7F0E8',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+    borderColor: '#F7F0E8'
+  },
+  taskTitle: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  taskName: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  });
+  
+
+export default OngoingTasks
